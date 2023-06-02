44f0f8bf0cb1f6ac4caa2352efbf01dc5ee250d4
Kohányi Róbert <kohanyi.robert@gmail.com>
2023-06-02 23:25:01 +0200

    Add WebApplication2

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("Index")]
+    public string Index()
+    {
+        return "Everybody can see this without login.";
+    }
+
+    [HttpGet("UserSecret")]
+    [Authorize]
+    public string UserSecret()
+    {
+        return "Users and Admins can see this after login.";
+    }
+
+    [HttpGet("AdminSecret")]
+    [Authorize(Roles = "Admin")]
+    public string AdminSecret()
+    {
+        return "Only Admins can see this after login.";
+    }
+}
