fbfb0a8e56a8450446c629dc11c5be5b78f553e4
Kohányi Róbert <kohanyi.robert@gmail.com>
2023-06-05 22:36:45 +0200

    Add fourth example

---
+++ WebApplication4/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ActionName("Login")]
+    public async Task<IActionResult> PostLogin(LoginViewModel model)
+    {
+        if (model is not ({Email: "admin@admin", Password: "admin"} or {Email: "user@user", Password: "user"}))
+        {
+            ModelState.AddModelError("InvalidLogin", "Invalid login credentials");
+            return View();
+        }
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(new ClaimsIdentity(
+                new[]
+                {
+                    new Claim(ClaimTypes.Email, model.Email),
+                    new Claim(ClaimTypes.Role, model.Email == "admin@admin" ? "Admin" : "User"),
+                }
+                , CookieAuthenticationDefaults.AuthenticationScheme
+            )),
+            new AuthenticationProperties()
+        );
+        return Redirect("/");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return Redirect("/");
+    }
+}
